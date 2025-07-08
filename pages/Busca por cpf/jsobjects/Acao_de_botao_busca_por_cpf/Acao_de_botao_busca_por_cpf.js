7cb81a51bef49f117b81f41f9ff5eae48065a93c
Beta Rodolfo <amaralrodolfo38@gmail.com>
2025-07-08 20:52:02 +0000

    não dê ruim, por favor

--- pages/Busca por cpf/jsobjects/Acao_de_botao_busca_por_cpf/Acao_de_botao_busca_por_cpf.js
+++ pages/Busca por cpf/jsobjects/Acao_de_botao_busca_por_cpf/Acao_de_botao_busca_por_cpf.js
@@ -1,7 +1,13 @@
 export default {
 	buscarConcursos: async (cpfToConsult) => {
 		try {
+			if(cpfToConsult.charAt(3)!="."&&cpfToConsult.charAt(6)!="."&&cpfToConsult.charAt(9)!="-"&&cpfToConsult.leght!=14){
+				showAlert("Cpf em formato inválido, use formato 123.456.789-00", "error");
+				await GET_CONCURSOS_POR_PROFISSAO.run();
+				return;
+			}
 			const cpf = cpfToConsult.trim();
+
 
 
 
