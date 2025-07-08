b3ba6225744ce9f9643d9886cdbd06c401ceed19
RodolfoVerdandi <87245089+RodolfoVerdandi@users.noreply.github.com>
2025-07-08 17:20:02 -0300

    Update concursos.test.js

--- backend/__tests__/concursos.test.js
+++ backend/__tests__/concursos.test.js
@@ -1,19 +1,9 @@
 const request = require('supertest');
 const app = require('../index');
 
-let server;
-
-beforeAll(() => {
-  server = app.listen(3000);
-});
-
-afterAll((done) => {
-  server.close(done); // Impede travamento no GitHub Actions
-});
-
 describe('GET /concursos', () => {
   it('deve retornar 200 e um array', async () => {
-    const res = await request(server).get('/concursos');
+    const res = await request(app).get('/concursos');
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
   });
