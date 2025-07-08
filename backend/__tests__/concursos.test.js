8c4dc233b7d9cd9859dc43074aee66f4ea2d05c4
RodolfoVerdandi <87245089+RodolfoVerdandi@users.noreply.github.com>
2025-07-08 17:42:22 -0300

    Update concursos.test.js

--- backend/__tests__/concursos.test.js
+++ backend/__tests__/concursos.test.js
@@ -3,7 +3,7 @@ const app = require('../index');
 
 describe('GET /concursos', () => {
   it('deve retornar 200 e um array', async () => {
-    const res = await request(app).get('/concursos?capacidades=foo,bar');
+    const res = await request(app).get('/concursos?capacidades=marceneiro');
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
   });
