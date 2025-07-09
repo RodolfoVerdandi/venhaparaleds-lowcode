228a41dccdeb3f0df376a08fdc3f3cb07935c5bb
Beta Rodolfo <amaralrodolfo38@gmail.com>
2025-07-09 02:46:11 +0000

    remoção de queries

--- pages/Busca por cpf/jsobjects/Acao_de_botao_busca_por_cpf/Acao_de_botao_busca_por_cpf.js
+++ pages/Busca por cpf/jsobjects/Acao_de_botao_busca_por_cpf/Acao_de_botao_busca_por_cpf.js
@@ -1,6 +1,7 @@
 export default {
 	buscarConcursos: async (cpfToConsult) => {
 		try {
+			// checa se o cpf está no formato certo
 			if(cpfToConsult.charAt(3)!="."&&cpfToConsult.charAt(6)!="."&&cpfToConsult.charAt(9)!="-"&&cpfToConsult.leght!=14){
 				showAlert("Cpf em formato inválido, use formato 123.456.789-00", "error");
 				await GET_CONCURSOS_POR_PROFISSAO.run();
@@ -13,7 +14,7 @@ export default {
 
 			// Executa a API (o parâmetro "cpf" será injetado na URL)
 			await GET_PROFISSAO_POR_CPF.run();
-
+			// Teste de error
 			if (!GET_PROFISSAO_POR_CPF.data?.length) {
 				showAlert("Nenhum candidato encontrado", "error");
 				await GET_CONCURSOS_POR_PROFISSAO.run();
@@ -22,6 +23,7 @@ export default {
 
 			// Se chegou aqui, a consulta funcionou!
 			showAlert("Dados carregados ! ", "success");
+			// chama requisição (parametros preenchido automaticamente com base na requisição anterior)
 			await GET_CONCURSOS_POR_PROFISSAO.run();
 			if (!GET_CONCURSOS_POR_PROFISSAO.data?.length) {
 				showAlert("Nenhum concurso para este candidato", "error");
--- pages/Busca por cpf/jsobjects/Acao_texto_do_input_alterado/Acao_texto_do_input_alterado.js
+++ pages/Busca por cpf/jsobjects/Acao_texto_do_input_alterado/Acao_texto_do_input_alterado.js
@@ -1,5 +1,6 @@
 export default {
 	Input1onTextChanged () {
+		// tira visibilidade da tabela e do 
 		Concursos_achados.setVisibility(false)
 		Capacidades_do_cpf.setVisibility(false)
 	}
