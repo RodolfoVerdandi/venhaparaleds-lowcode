bcc0d5cbedd24bd4996b64d685588741a82d106c
rodolfo muller <amaralrodolfo@yahoo.com>
2025-07-09 14:20:33 -0300

    teste corrigido para codificar url

--- backend/__tests__/concursos.behavior.test.js
+++ backend/__tests__/concursos.behavior.test.js
@@ -2,31 +2,32 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('Teste comportamental da rota /concursos', () => {
-  it('deve retornar concursos com a capacidade "padeiro"', async () => {
-    const res = await request(app).get('/concursos?capacidades=padeiro');
+    it('deve retornar concursos com a capacidade "padeiro"', async () => {
+        const res = await request(app).get('/concursos?capacidades=padeiro');
 
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
+        expect(res.statusCode).toBe(200);
+        expect(Array.isArray(res.body)).toBe(true);
 
-    const contem = res.body.some(c => c.lista_de_vagas.includes('padeiro'));
-    expect(contem).toBe(true);
-  });
+        const contem = res.body.some(c => c.lista_de_vagas.includes('padeiro'));
+        expect(contem).toBe(true);
+    });
 
-  it('deve retornar concursos com a capacidade "professor de matemática"', async () => {
-    const res = await request(app).get('/concursos?capacidades=professor de matemática');
+    it('deve retornar concursos com a capacidade "professor de matemática"', async () => {
+        const capacidade = encodeURIComponent('professor de matemática');
+        const res = await request(app).get(`/concursos?capacidades=${capacidade}`);
 
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
+        expect(res.statusCode).toBe(200);
+        expect(Array.isArray(res.body)).toBe(true);
 
-    const contem = res.body.some(c => c.lista_de_vagas.includes('professor de matemática'));
-    expect(contem).toBe(true);
-  });
+        const contem = res.body.some(c => c.lista_de_vagas.includes('professor de matemática'));
+        expect(contem).toBe(true);
+    });
 
-  it('deve retornar array vazio quando não houver nenhuma correspondência', async () => {
-    const res = await request(app).get('/concursos?capacidades=inexistente');
+    it('deve retornar array vazio quando não houver nenhuma correspondência', async () => {
+        const res = await request(app).get('/concursos?capacidades=inexistente');
 
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toBe(0);
-  });
+        expect(res.statusCode).toBe(200);
+        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body.length).toBe(0);
+    });
 });
